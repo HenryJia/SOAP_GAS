e181217a0e1d9e74f78d00b94046601f7d615b3a
Hengjian Jia <henryjia18@gmail.com>
2022-11-24 04:16:57 +0000

    cleanup

--- convert_xyz.py
+++ convert_xyz.py
@@ -1,8 +1,10 @@
 import os
-import sys
 from argparse import ArgumentParser
 import warnings
 warnings.filterwarnings("error")
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
 import pandas as pd
 
@@ -15,20 +17,28 @@ ob.obErrorLog.SetOutputLevel(ob.obError)
 parser = ArgumentParser(description='Convert smiles to xyz')
 parser.add_argument('--input', help='input file containing smiles and molecule names')
 parser.add_argument('--output', help='output directory')
+parser.add_argument('--output_df', help='output dataframe')
 
 args = parser.parse_args()
 
 df = pd.read_csv(args.input)
+df_out = pd.DataFrame(columns=df.columns)
 
-#for i, row in tqdm(df.iterrows(), total=df.shape[0]):
-message_counter = 0
-for i, row in df.iterrows():
-    mol = pb.readstring('smi', row['smiles'])
-    mol.addh()
-    mol.make3D()
-    mol.write('xyz', os.path.join(args.output, str(row['num']) + '.xyz'), overwrite=True)
-    if ob.obErrorLog.GetWarningMessageCount() > message_counter:
-        print('\nProcessing molecule {}. Molecule name and SMILES: {} {}'.format(row['num'], row['name'], row['smiles']))
-        print(ob.obErrorLog.GetMessagesOfLevel(ob.obWarning)[-1])
-        ob.obErrorLog.ClearLog()
-        message_counter = ob.obErrorLog.GetWarningMessageCount()
+for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+    try:
+        mol = Chem.MolFromSmiles(row['smiles'])
+        mol = Chem.AddHs(mol)
+        success = AllChem.EmbedMolecule(mol, randomSeed=0xf00d)
+        out = Chem.MolToXYZBlock(mol)
+
+        if success != 0:
+            raise
+
+        with open(os.path.join(args.output, str(row['num']) + '.xyz'), 'w') as f:
+            f.write(out)
+
+        df_out = pd.concat([df_out, row.to_frame().T])
+    except:
+        print('Embedding failed for {} {}'.format(row['name'], row['smiles']))
+
+df_out.to_csv(args.output_df, index=False)
